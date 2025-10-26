9f1b7e222210eb3752c7a9f6e81e179aee49df36
Martín <xxmartinxxgames@gmail.com>
2025-10-26 16:31:36 -0300

    Se termino de agregar ubicaciones a los scooters en js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
-function mostrarModal() {
-  document.getElementById("scooterModal").classList.add("activo");
-}
+const scooterData = {
+    "santiago": [
+        {id: "S100", status: "Disponible", lat: -33.445890, lon: -70.652635, image: "https://placehold.co/100x100/78909C/ffffff?text=S100", battery: "100%"},
+        {id: "S101", status: "Disponible", lat: -33.470275, lon: -70.658510, image: "https://placehold.co/100x100/78909C/ffffff?text=S101", battery: "100%"},
+        {id: "S102", status: "En uso", lat: -33.466665,lon: -70.671575, image: "https://placehold.co/100x100/78909C/ffffff?text=S102", battery: "85%"},
+        {id: "S103", status: "En uso", lat: -33.457024, lon: -70.661652, image: "https://placehold.co/100x100/78909C/ffffff?text=S103", battery: "31%"},
+        {id: "S104", status: "Bateria Baja", lat: -33.453234, lon: -70.656803, image: "https://placehold.co/100x100/78909C/ffffff?text=S104", battery: "4%"},
+        {id: "S105", status: "Descargado", lat: -33.452293, lon: -70.658256, image: "https://placehold.co/100x100/78909C/ffffff?text=S105", battery: "0%"},
+        {id: "S106", status: "Bateria Baja", lat: -33.453085, lon: -70.660419, image: "https://placehold.co/100x100/78909C/ffffff?text=S106", battery: "10%"},
+        {id: "S107", status: "En uso", lat: -33.453099, lon: -70.660507, image: "https://placehold.co/100x100/78909C/ffffff?text=S107", battery: "60%"},
+        {id: "S108", status: "Disponible", lat: -33.446251, lon: -70.662528, image: "https://placehold.co/100x100/78909C/ffffff?text=S108", battery: "70%"},
+        {id: "S109", status: "Bateria Baja", lat: -33.446256, lon: -70.662480, image: "https://placehold.co/100x100/78909C/ffffff?text=S109", battery: "30%"},
+        {id: "S110", status: "Disponible", lat: -33.452577, lon: -70.657129, image: "https://placehold.co/100x100/78909C/ffffff?text=S110", battery: "90%"},
+        {id: "S111", status: "En mantenimiento", lat: -33.452552, lon: -70.657224, image: "https://placehold.co/100x100/78909C/ffffff?text=S111", battery: "25%"},
+        {id: "S112", status: "Disponible", lat: -33.452584, lon: -70.657091, image: "https://placehold.co/100x100/78909C/ffffff?text=S112", battery: "70%"},
+        {id: "S113", status: "En uso", lat: -33.450738, lon: -70.657519, image: "https://placehold.co/100x100/78909C/ffffff?text=S113", battery: "100%"},
+        {id: "S114", status: "En uso", lat: -33.449351, lon: -70.650678, image: "https://placehold.co/100x100/78909C/ffffff?text=S114", battery: "65%"}
+    ],
+    "providencia": [
+        {id: "S200", status: "", lat: -33.438536, lon: -70.607503, image: "https://placehold.co/100x100/78909C/ffffff?text=S200", battery: "%"},
+        {id: "S201", status: "", lat: -33.438542, lon: -70.607626, image: "https://placehold.co/100x100/78909C/ffffff?text=S201", battery: "%"},
+        {id: "S202", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S202", battery: "%"},
+        {id: "S203", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S203", battery: "%"},
+        {id: "S204", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S204", battery: "%"},
+        {id: "S205", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S205", battery: "%"},
+        {id: "S206", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S206", battery: "%"},
+        {id: "S207", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S207", battery: "%"},
+        {id: "S208", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S208", battery: "%"},
+        {id: "S209", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S209", battery: "%"},
+        {id: "S210", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S210", battery: "%"},
+        {id: "S211", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S211", battery: "%"},
+        {id: "S212", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S212", battery: "%"},
+        {id: "S213", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S213", battery: "%"},
+        {id: "S214", status: "", lat: , lon: , image: "https://placehold.co/100x100/78909C/ffffff?text=S214", battery: "%"}
+    ]
 
-window.onload = mostrarModal;
+}
